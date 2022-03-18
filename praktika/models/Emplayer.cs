098eadb333eeb6936d07487057b1c361cd4d1daf
Ali Aliyev <tu4pw6wq5@aztu.local>
2022-03-18 20:13:52 +0400

    added

---
+++ praktika/models/Emplayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace praktika.models
+{
+    class Emplayer:Person
+    {
+        public double SalaryOfHour { get; set; }
+        public double WorkingHour { get; set; }
+        public Emplayer(double salaryofhour, double workinghour,string name,string surname,int age):base(name,surname,age)
+        {
+            this.SalaryOfHour = salaryofhour;this.WorkingHour = workinghour;
+        }
+        public void CalculateSalary()
+        {
+
+        }
+
+
+    }
+}
---
+++ praktika/models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace praktika.models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Person(string name ,string surname,int age)
+        {
+            this.Name = name;this.Surname = surname; this.Age = age;
+        }
+    }
+}
